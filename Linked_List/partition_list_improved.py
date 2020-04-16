69d37c2e0b415483b62106789838e2096cb59858
asperaa <adityaankur44@gmail.com>
2020-04-16 20:01:35 +0530

    feat: [86. Partition List] Improved

---
+++ Linked_List/partition_list_improved.py
@@ -0,0 +1,48 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+"""86. Partition List
+"""
+
+class ListNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+    
+    def push(self, val):
+        new_node = ListNode(val)
+        if not self.head:
+            self.head = new_node
+            return
+        mover = self.head
+        while mover.next:
+            mover = mover.next
+        mover.next = new_node
+    
+    def print_list(self, head):
+        mover = head
+        while mover:
+            print(mover.val, end=" ")
+            mover = mover.next
+        print()
+    
+    def partition(self, head, x):
+        h1 = l1 = ListNode(0)
+        h2 = l2 = ListNode(0)
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+        l2.next = None
+        l1.next = h2.next
+        return h1.next
