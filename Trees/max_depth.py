70f59ea43efc390a895a4d69059cbd9505418c63
asperaa <adityaankur44@gmail.com>
2020-04-21 20:16:12 +0530

    feat: [104. Maximum Depth of Binary Tree [Recursive]]

---
+++ Trees/max_depth.py
@@ -0,0 +1,24 @@
+"""We are the captains of our ship, and we stay 'till the end. We see our stories through.
+"""
+
+"""104. Maximum Depth of Binary Tree [Recursive]
+"""
+
+class TreeNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+    
+    def max_depth_helper(self, root):
+        if not root:
+            return 0
+        l_height = 1 + self.max_depth_helper(root.left)
+        r_height = 1 + self.max_depth_helper(root.right)
+        return 1 + max(l_height, r_height)
+
+    def maxDepth(self, root):
+        self.max_depth_helper(root)
