7cd93de0b53a71b6983e887779406e3c9f370f35
asperaa <adityaankur44@gmail.com>
2020-04-23 15:30:39 +0530

    feat: [112. Path Sum [No Class Variable Use]]

---
+++ Trees/has_path_sum_non_class_var.py
@@ -0,0 +1,19 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+class TreeNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def hasPathSum(self, root, summ):
+        if not root:
+            return False
+        if not root.left and not root.right:
+            summ -= root.val
+            return summ == 0
+        return self.hasPathSum(root.left, summ - root.val) or self.hasPathSum(root.right, summ - root.val)
