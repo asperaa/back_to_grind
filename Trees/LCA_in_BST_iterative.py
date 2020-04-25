fd9352202048b38ac07062a561f570eddb8cfa58
asperaa <adityaankur44@gmail.com>
2020-04-25 16:00:00 +0530

    feat: [235. Lowest Common Ancestor of a Binary Search Tree [Iterative]. T - O(n) and S - O(1)]

---
+++ Trees/LCA_in_BST_iterative.py
@@ -0,0 +1,24 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+"""235. Lowest Common Ancestor of a Binary Search Tree [Iterative]
+"""
+
+class TreeNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def lowestCommonAncestor(self, root, x, y):
+        while root:
+            if x.val < root.val and y.val < root.val:
+                root = root.left
+            elif x.val > root.val and y.val > root.val:
+                root = root.right
+            else:
+                break
+        return root
