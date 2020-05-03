14785c9342e11add9d64ce9a09aeb1f8564a9930
asperaa <adityaankur44@gmail.com>
2020-05-04 02:04:16 +0530

    feat: [35. Search Insert Position]

---
+++ Binary_Search/35. Search Insert Position.py
@@ -0,0 +1,18 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+"""35. Search Insert Position
+"""
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right-left) // 2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
