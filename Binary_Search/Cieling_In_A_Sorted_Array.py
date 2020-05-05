0b9e06e5a98de66b59dcb1825f9e9b9aaffee33a
asperaa <adityaankur44@gmail.com>
2020-05-05 15:09:57 +0530

    feat: [Ceiling in a sorted array]

--- Binary_Search/Cieling_In_A_Sorted_Array.py
+++ Binary_Search/Cieling_In_A_Sorted_Array.py
@@ -1,19 +1,19 @@
 """We are the captains of our ships, and we stay 'till the end. We see our stories through.
 """
 
-"""https://www.geeksforgeeks.org/floor-in-a-sorted-array/
+"""https://www.geeksforgeeks.org/ceiling-in-a-sorted-array/
 """
 
-def floor_in_sorted(nums, target, length):
-    left, right = 0, length - 1
+def ceiling_in_sorted(nums, target):
+    left, right = 0, len(nums) - 1
     result = -1
     while left <= right:
-        mid = left + (right-left) // 2
+        mid = left + (right - left) // 2
         if nums[mid] == target:
             return mid
-        elif nums[mid] < target:
+        elif nums[mid] > target:
             result = mid
-            left = mid + 1
-        else:
             right = mid - 1
+        else:
+            left = mid + 1
     return result
