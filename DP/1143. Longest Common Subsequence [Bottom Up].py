a7b50745de00c6e76b2c5b47303d4a004ff9aa30
asperaa <adityaankur44@gmail.com>
2020-05-23 03:50:22 +0530

    correction: LCS Bottom Up

--- DP/1143. Longest Common Subsequence [Bottom Up].py
+++ DP/1143. Longest Common Subsequence [Bottom Up].py
@@ -15,7 +15,7 @@ class Solution:
             dp[0][j] = 0
         for i in range(1, m+1):
             for j in range(1, n+1):
-                if text1[i] == text2[j]:
+                if text1[i-1] == text2[j-1]:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
