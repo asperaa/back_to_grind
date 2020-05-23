17a84f5d1fa4096b5ebca379297ead11bd31289e
asperaa <adityaankur44@gmail.com>
2020-05-23 17:39:46 +0530

    feat: [583. Delete Operation for Two Strings]

---
+++ DP/583. Delete Operation for Two Strings.py
@@ -0,0 +1,18 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+"""583. Delete Operation for Two Strings
+"""
+
+class Solution:
+
+    def minDistance(self, text1, text2):
+        m, n = len(text1), len(text2)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return m + n - 2 * dp[m][n] 
