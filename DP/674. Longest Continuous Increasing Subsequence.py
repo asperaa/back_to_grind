16c1f961f21821184eb86422faff81ac2c0dbbb8
asperaa <adityaankur44@gmail.com>
2020-05-28 12:26:30 +0530

    feat: [674. Longest Continuous Increasing Subsequence]

---
+++ DP/674. Longest Continuous Increasing Subsequence.py
@@ -0,0 +1,14 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+class Solution:
+
+    def findLengthOfLCIS(self, nums):
+        maxx = curr_len = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                curr_len += 1
+            else:
+                curr_len = 1
+            maxx = max(maxx, curr_len)
+        return maxx 
