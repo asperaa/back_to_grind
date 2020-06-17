18670c1bd4bae6da5754edf753c42adde1df57fb
asperaa <adityaankur44@gmail.com>
2020-06-17 18:08:17 +0530

    feat: [247. Strobogrammatic Number II]

--- DP/248. Strobogrammatic Number III.py
+++ DP/248. Strobogrammatic Number III.py
@@ -1,16 +1,14 @@
 """We are the captains of our ships, and we stay 'till the end. We see our stories through.
 """
 
-"""247. Strobogrammatic Number II
+"""248. Strobogrammatic Number III
 """
 
 class Solution:
-    
+
     def strobogrammaticInRange(self, low, high):
-        self.ans = 0
+        self.ans = []
         self.hash = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
-        self.low = int(low)
-        self.high = int(high)
         for i in range(len(low), len(high)+1):
             num = [None] * i
             self.helper(0, i-1, num)
@@ -18,12 +16,10 @@ class Solution:
     
     def helper(self, start, end, num):
         if start > end:
-            sto_num = int("".join(num))
-            if sto_num >= self.low and sto_num <= self.high:
-                self.ans += 1
+            self.ans.append("".join(num))
             return
         for key in self.hash:
-            if start == end and key in ("6", "9"):
+            if start == end and key in (6, 9):
                 continue
             if start != end and start == 0 and key == "0":
                 continue
