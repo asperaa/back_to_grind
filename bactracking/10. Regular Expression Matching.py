544ec98692cba3a329b5e446be7597f0530954fc
asperaa <adityaankur44@gmail.com>
2020-06-19 21:39:58 +0530

    feat: [10. Regular Expression Matching]

---
+++ bactracking/10. Regular Expression Matching.py
@@ -0,0 +1,22 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+"""10. Regular Expression Matching
+"""
+
+class Solution:
+    
+    def isMatch(self, s, p):
+        table = [[False for _ in range(len(s)+1)]for _ in range(len(p)+1)]
+        table[0][0] = True
+        for i in range(2, len(p)+1):
+            table[i][0] = table[i-2][0] and p[i-1] == "*"
+        for i in range(1, len(p)+1):
+            for j in range(1, len(s)+1):
+                if p[i-1] != "*":
+                    table[i][j] = table[i-1][j-1] and (p[i-1] == s[j-1] or p[i-1] == ".")
+                else:
+                    table[i][j] = table[i-2][j] or table[i-1][j]
+                    if p[i-2] == s[j-1] or p[i-2] == ".":
+                        table[i][j] = table[i][j] or table[i][j-1]
+        return table[-1][-1]
