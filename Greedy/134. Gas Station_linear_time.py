da30a4952934bcb89a7a523f7525bbf30a711267
asperaa <adityaankur44@gmail.com>
2020-07-02 21:14:07 +0530

    feat: [134. Gas Station [T - O(n)]]

---
+++ Greedy/134. Gas Station_linear_time.py
@@ -0,0 +1,22 @@
+"""We are the captains of our ships, and we stay 'till the end. We see our stories through.
+"""
+
+"""134. Gas Station
+"""
+
+class Solution:
+    
+    def canCompleteCircuit(self, gas, cost):
+        curr_tank = 0
+        n = len(gas)
+        ans = 0
+        total_gas = 0
+        curr_gas = 0
+        for i in range(n):
+            remaining_gas = gas[i] - cost[i]
+            curr_gas += remaining_gas
+            total_gas += remaining_gas
+            if curr_gas < 0:
+                curr_gas = 0
+                ans = i + 1
+        return ans if total_gas >= 0 else -1
