46ca4f5a1011f16e0c5ea5cdf1c6eed3c0a956e1
asperaa <adityaankur44@gmail.com>
2024-11-18 13:56:42 +0530

    rod cut correction

--- DP/dp_prac/rod-cutting-bottom-up.py
+++ DP/dp_prac/rod-cutting-bottom-up.py
@@ -10,7 +10,7 @@ def rod_cutting(prices):
     for i in range(1, n+1):
         for j in range(1, n+1):
             if length[i-1] <= j:
-                dp[i][j] = max(dp[i][j-length[i-1]], dp[i-1][j])
+                dp[i][j] = max(prices[i-1] + dp[i][j-length[i-1]], dp[i-1][j])
             else:
                 dp[i][j] = max(dp[i-1][j])
     return dp[n][n]
