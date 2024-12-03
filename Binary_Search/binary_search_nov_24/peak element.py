99824d51ee11f626c11f17e5c232c0a26a77de3b
asperaa <adityaankur44@gmail.com>
2024-12-03 17:25:57 +0530

    peak element edit

--- Binary_Search/binary_search_nov_24/peak element.py
+++ Binary_Search/binary_search_nov_24/peak element.py
@@ -12,8 +12,16 @@ def peak_element(nums):
         
         if mid > 1 and nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1] and mid < n-1:
             return mid
+        elif mid == 0:
+            if nums[mid] > nums[mid+1]:
+                return mid
+        elif mid == len(nums)-1:
+            if nums[mid] > nums[mid-1]:
+                return mid 
         elif nums[mid] < nums[mid+1]:
             left = mid + 1
         else:
             right = mid - 1
+            
+        
     return -1
